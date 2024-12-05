3a776968f71437d33b85df98fd9fcd15bb37bc4e
Weston Clark <westoclark@gmail.com>
2024-12-05 13:04:34 -0800

    update email link

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Contact</h1>
       <p>{`Feel free to reach out with any inquiries or questions.`}</p>
       <a
-        href="mailto:westonclark@gmail.com"
+        href="mailto:westoclark@gmail.com"
         className="text-neutral-600 dark:text-neutral-400 hover:text-neutral-900 hover:dark:text-neutral-100 transition tabular-nums"
       >
         westoclark@gmail.com
