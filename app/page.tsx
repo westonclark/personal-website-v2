ad798fb72b624fe49c66196b489be0d4a317c108
Weston Clark <westoclark@gmail.com>
2025-03-27 23:00:51 -0700

    Update mobile spacing

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import { Projects } from "./components/projects";
 export default function Page() {
   return (
     <section className="flex flex-col h-full">
-      <div className="pt-48 mb-60 md:pt-0 md:mb-0 md:min-h-screen flex items-center justify-center animate-fade-in" style={{ animationDelay: "100ms" }}>
+      <div className="pt-48 mb-48 md:pt-0 md:mb-0 md:min-h-screen flex items-center justify-center animate-fade-in" style={{ animationDelay: "100ms" }}>
         <About />
       </div>
-      <div className="mb-60 md:mb-0 md:min-h-screen flex items-center justify-center animate-fade-in" style={{ animationDelay: "200ms" }}>
+      <div className="mb-48 md:mb-0 md:min-h-screen flex items-center justify-center animate-fade-in" style={{ animationDelay: "200ms" }}>
         <Experience />
       </div>
       <div className="pb-48 md:pb-32 md:min-h-screen flex items-center justify-center animate-fade-in" style={{ animationDelay: "300ms" }}>
