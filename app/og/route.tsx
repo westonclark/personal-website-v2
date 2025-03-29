a05022a8b85e2915b6349a7b697a112f73fb7169
Weston Clark <westoclark@gmail.com>
2025-03-28 20:37:20 -0700

    Remove blog files

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,23 +1,23 @@
 import { ImageResponse } from "next/og";
 
 export function GET(request: Request) {
-    let url = new URL(request.url);
-    let title = url.searchParams.get("title") || "Weston Clark";
+  let url = new URL(request.url);
+  let title = url.searchParams.get("title") || "Weston Clark";
 
-    return new ImageResponse(
-        (
-            <div tw="flex flex-col w-full h-full items-center justify-center bg-black">
-                <div tw="flex flex-col w-full h-full items-center justify-center px-4">
-                    <div tw="flex flex-col max-w-4xl">
-                        <h1 tw="text-6xl font-bold text-white tracking-tight">{title}</h1>
-                        <p tw="text-2xl text-neutral-400 mt-4">Software Engineer</p>
-                    </div>
-                </div>
-            </div>
-        ),
-        {
-            width: 1200,
-            height: 630,
-        },
-    );
+  return new ImageResponse(
+    (
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-black">
+        <div tw="flex flex-col w-full h-full items-center justify-center px-4">
+          <div tw="flex flex-col max-w-4xl">
+            <h1 tw="text-6xl font-bold text-white tracking-tight">{title}</h1>
+            <p tw="text-2xl text-neutral-400 mt-4">Weston Clark</p>
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ export const baseUrl = "https://www.westonclark.dev";
 
 export default async function sitemap() {
 
-  let routes = ["", "/contact"].map((route) => ({
+  let routes = [""].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
