8b09ec1395f783c9654727f971b8b30d5857f059
Weston Clark <westoclark@gmail.com>
2025-04-10 21:51:00 -0700

    update spacing

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,22 @@ import { Projects } from "./components/projects";
 export default function Page() {
   return (
     <section className="flex flex-col h-full">
-      <div className="pt-48 mb-48 md:pt-0 md:mb-0 md:min-h-screen flex items-center justify-center animate-fade-in" style={{ animationDelay: "100ms" }}>
+      <div
+        className="pt-24 mb-36 md:mb-0 md:min-h-screen md:pt-48 flex items-center justify-center animate-fade-in"
+        style={{ animationDelay: "100ms" }}
+      >
         <About />
       </div>
-      <div className="mb-48 md:mb-0 md:min-h-screen flex items-center justify-center animate-fade-in" style={{ animationDelay: "200ms" }}>
+      <div
+        className="mb-36 md:mb-0 md:min-h-screen flex items-center justify-center animate-fade-in"
+        style={{ animationDelay: "200ms" }}
+      >
         <Experience />
       </div>
-      <div className="pb-48 md:pb-32 md:min-h-screen flex items-center justify-center animate-fade-in" style={{ animationDelay: "300ms" }}>
+      <div
+        className="pb-24 md:pb-32 md:min-h-screen flex items-center justify-center animate-fade-in"
+        style={{ animationDelay: "300ms" }}
+      >
         <Projects />
       </div>
     </section>
